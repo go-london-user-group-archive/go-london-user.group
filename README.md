e1c84fcfd373693bcc8aacdd25bc5d4cd7ac3c96
Florin Pățan <florinpatan@gmail.com>
2016-06-05 14:10:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gophers.london
+Our website
---
+++ gophers.go
@@ -0,0 +1,24 @@
+package gophers
+
+import "net/http"
+
+func init() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/talks/signup", talksSignup)
+	http.HandleFunc("/events/next", eventsNext)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Location", "http://www.meetup.com/Go-London-User-Group/")
+	w.WriteHeader(302)
+}
+
+func talksSignup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Location", "https://goo.gl/IN3BWq")
+	w.WriteHeader(302)
+}
+
+func eventsNext(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Location", "https://skillsmatter.com/meetups/8186-london-go-usergroup")
+	w.WriteHeader(302)
+}
